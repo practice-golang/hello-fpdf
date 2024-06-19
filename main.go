f10eb3227104ee4dc52c74a41d185b07f3dddf8b
Practice Golang <practice.golang@gmail.com>
2024-06-19 22:09:08 +0900

    misc.

--- main.go
+++ main.go
@@ -8,7 +8,124 @@ import (
 	"github.com/go-pdf/fpdf"
 )
 
+type PageDefinition struct {
+	PageWidth      float64
+	PageHeight     float64
+	MarginHorizon  float64 // single side Horizontal margin. should use (MargineX x 2) value when actual area get.
+	MarginVertical float64 // single side Vertical margin. should use (MargineX x 2) value when actual area get.
+	ActualWidth    float64 // PageWidth - (MarginHorizon x 2)
+	ActualHeight   float64 // PageHeight - (MarginVertical x 2)
+}
+
 var data [][]string = [][]string{
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
+	{"2", "aa", "2.30"},
+	{"5", "bb", "5.63"},
+	{"10", "cc", "3.63"},
 	{"2", "aa", "2.30"},
 	{"5", "bb", "5.63"},
 	{"10", "cc", "3.63"},
@@ -49,14 +166,26 @@ var data [][]string = [][]string{
 
 var pdf *fpdf.Fpdf
 
-var (
-	marginX float64 = 10.0
-	marginY float64 = 20.0
-	gapY    float64 = 2.0
+var pageDefs PageDefinition
 
-	lineHt   float64   = 10.0
-	colWidth []float64 = []float64{10.0, 75.0, 25.0, 40.0, 40.0}
+var (
+	gapY   float64 = 2.0
+	lineHt float64 = 10.0
+
+	// tableColumnsWidthBase []float64 = []float64{10.0, 75.0, 25.0, 40.0, 40.0} // 190
+	tableColumnsWidthBase []float64 = []float64{5.26, 39.47, 13.16, 21.05, 21.05} // 100
+	colWidth              []float64
 )
+
+func recalcTableColumnsWidth(tableColumnWidths []float64, pageDef PageDefinition) []float64 {
+	result := make([]float64, 0)
+
+	for _, w := range tableColumnWidths {
+		result = append(result, w/100*pageDef.ActualWidth)
+	}
+
+	return result
+}
 
 func printTableHeader() {
 	header := []string{"No", "설명", "수량", "단가 ($)", "총금액 ($)"}
@@ -73,7 +202,7 @@ func printTableHeader() {
 func printTableData(subtotal float64) float64 {
 	pdf.SetFontStyle("")
 	_, pageSizeH := pdf.GetPageSize()
-	lastLineY := pageSizeH - marginY - lineHt
+	lastLineY := pageSizeH - pageDefs.MarginVertical - lineHt
 
 	for i := 0; i < len(data); i++ {
 		val := data[i]
@@ -97,8 +226,8 @@ func printTableData(subtotal float64) float64 {
 		pdf.CellFormat(colWidth[4], lineHt, fmt.Sprintf("%.2f", totalPrice), "1", 0, "CM", false, 0, "")
 		pdf.Ln(-1)
 
-		fmt.Println(pdf.GetY(), lastLineY)
-		if pdf.GetY() > lastLineY {
+		fmt.Println("no Y:", pdf.GetY(), math.Floor(lastLineY*100)/100)
+		if pdf.GetY() >= math.Floor(lastLineY*100)/100 {
 			printTableHeader()
 			pdf.SetFontStyle("")
 		}
@@ -108,9 +237,20 @@ func printTableData(subtotal float64) float64 {
 }
 
 func main() {
-
 	// pdf = fpdf.New("L", "mm", "A4", "")
 	pdf = fpdf.New("P", "mm", "A4", "")
+
+	pageSizeW, pageSizeH := pdf.GetPageSize()
+	fmt.Printf("Page size: %2f , %2f\n", pageSizeW, pageSizeH)
+
+	pageDefs.PageWidth = pageSizeW
+	pageDefs.PageHeight = pageSizeH
+	pageDefs.MarginHorizon = 10
+	pageDefs.MarginVertical = 20
+	pageDefs.ActualWidth = pageDefs.PageWidth - pageDefs.MarginHorizon*2
+	pageDefs.ActualHeight = pageDefs.PageHeight - pageDefs.MarginVertical*2
+
+	colWidth = recalcTableColumnsWidth(tableColumnsWidthBase, pageDefs)
 
 	// https://pkg.go.dev/github.com/jung-kurt/gofpdf#Fpdf.AddUTF8Font
 	pdf.AddUTF8Font("gamtan", "", "fonts/gamtan_road_dotum-regular.ttf")
@@ -118,7 +258,7 @@ func main() {
 	pdf.AddUTF8Font("gamtan", "I", "fonts/gamtan_road_dotum-regular.ttf")
 	pdf.AddUTF8Font("gamtan", "BI", "fonts/gamtan_road_dotum-bold.ttf")
 
-	pdf.SetMargins(marginX, marginY, marginX)
+	pdf.SetMargins(pageDefs.MarginHorizon, pageDefs.MarginVertical, pageDefs.MarginHorizon)
 	pdf.AddPage()
 
 	// Set footer for page
@@ -129,7 +269,7 @@ func main() {
 	})
 
 	pageW, _ := pdf.GetPageSize()
-	safeAreaW := pageW - 2*marginX
+	safeAreaW := pageW - 2*pageDefs.MarginHorizon
 
 	pdf.ImageOptions("assets/gopher.png", 10, 10, 30, 40, false, fpdf.ImageOptions{ImageType: "PNG", ReadDpi: true}, 0, "")
 
@@ -165,19 +305,19 @@ func main() {
 	for i := 0; i < 3; i++ {
 		leftIndent += colWidth[i]
 	}
-	pdf.SetX(marginX + leftIndent)
+	pdf.SetX(pageDefs.MarginHorizon + leftIndent)
 	pdf.CellFormat(colWidth[3], lineHt, "금액 합계", "1", 0, "CM", false, 0, "")
 	pdf.CellFormat(colWidth[4], lineHt, fmt.Sprintf("%.2f", subtotal), "1", 0, "CM", false, 0, "")
 	pdf.Ln(-1)
 
 	taxAmount := math.Round(subtotal*float64(20)) / 100
-	pdf.SetX(marginX + leftIndent)
+	pdf.SetX(pageDefs.MarginHorizon + leftIndent)
 	pdf.CellFormat(colWidth[3], lineHt, "부가세", "1", 0, "CM", false, 0, "")
 	pdf.CellFormat(colWidth[4], lineHt, fmt.Sprintf("%.2f", taxAmount), "1", 0, "CM", false, 0, "")
 	pdf.Ln(-1)
 
 	grandTotal := subtotal + taxAmount
-	pdf.SetX(marginX + leftIndent)
+	pdf.SetX(pageDefs.MarginHorizon + leftIndent)
 	pdf.CellFormat(colWidth[3], lineHt, "지불 합계", "1", 0, "CM", false, 0, "")
 	pdf.CellFormat(colWidth[4], lineHt, fmt.Sprintf("%.2f", grandTotal), "1", 0, "CM", false, 0, "")
 	pdf.Ln(-1)
